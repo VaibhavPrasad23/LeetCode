e45a13d1f60d785c282ccf72832da3d6e3af02c2
Vaibhav <91517399+VaibhavPrasad23@users.noreply.github.com>
2022-07-04 13:15:21 +0530

    Create Minimum XorA.java
    Given two integers A and B, the task is to find an integer X such that (X XOR A) is minimum possible and the count of set bit in X is equal to the count of set bits in B.
    
    Example 1:
    
    Input: 
    A = 3, B = 5
    Output: 3
    Explanation:
    Binary(A) = Binary(3) = 011
    Binary(B) = Binary(5) = 101
    The XOR will be minimum when x = 3
    i.e. (3 XOR 3) = 0 and the number of set bits in 3 is equal to the number of set bits in 5.
    
    ------------------------------------------------------------------------------------------
    
    BASIC SOLUTION - O(N logN) Time Complexity
    
    First calculate the count of no.of.set bits in A and B. Based on the number of set bits in A and B, we have three cases.
    We know that XOR of a number with itself is zero. Considering this two points , we can get the required number.
    CASE 1 : If the Number of Set bits in A and B are equal, then we can simply consider X=A because since the number of set bits in A and B are equal, then the number of set bits in X must also be equal to number of set bits in A . So If we take X=A , XOR will be minimum with the required condition of no.of.set bits.
    2. CASE 2: If the Number of set bits in A is greater than number of set bits in B. In this case , we try to minimise the XOR and maintain same no.of.set bits as in B, in that case we need to eliminate the 1's contributed by MSB's in XOR.
    
    3. CASE 3:If the Number of Set bits in A are lesser than that in B, simply set the bits of A from LSB until the number of set bits in A are equal to that of B. This is done because, we can't decrease the value of A, in order to increase set bits only thing we can do is to set the bits to right as much as possible to minimise the xor value.

---
+++ Minimum XorA.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            int a = Integer.parseInt(br.readLine().trim());
+            int b = Integer.parseInt(br.readLine().trim());
+
+            Solution ob = new Solution();
+            System.out.println(ob.minVal(a, b));
+        }
+    }
+}// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    
+    public static int countBits(int n)
+    {
+        int count = 0;
+        while(n > 0)
+        {
+            count = count + (n&1);
+            n  = n >> 1; 
+        }
+        return count;
+    }
+    public static int minVal(int a, int b) {
+        //Find number of set bit in B
+        int bitsA = countBits(a);
+        int bitsB = countBits(b);
+        int diff = Math.abs(bitsA-bitsB);
+        
+        if(bitsA == bitsB) return a;
+        else if( bitsA > bitsB)
+        {
+            //Convet 1 t 0;
+            while(diff > 0)
+            {
+                a = a & (a - 1);
+                diff --;
+            }
+        }
+        else
+        {
+            //Convert 0 to 1
+            while(diff > 0)
+            {
+                a = a | (a + 1);
+                diff --;
+            }
+        }
+      
+        return a;
+    }
+}
